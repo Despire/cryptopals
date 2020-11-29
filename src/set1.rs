a3ef9a57974af2a79662359c80a191ea0aff550b
Matúš Mrekaj <matus.mrekaj@icloud.com>
2020-11-29 13:14:10 +0100

    add solution for set1/problem2

--- src/set1.rs
+++ src/set1.rs
@@ -5,8 +5,26 @@ fn hex_to_b64(b: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
     Ok(base64::encode(hex::decode(b)?).into_bytes())
 }
 
+/// Fixed XOR for two byte buffers
+/// 
+/// Solution to problem 2 from set 1: <https://cryptopals.com/sets/1/challenges/2>
+fn xor_buffers(l: &[u8], r: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
+    assert_eq!(l.len(), r.len());
+
+    let l = hex::decode(l)?;
+    let r = hex::decode(r)?;
+    let mut result = Vec::new();
+
+    for i in 0..l.len() {
+        result.push(l[i]^r[i]);
+    }
+
+    Ok(result)
+}
+
 mod test {
     use super::hex_to_b64;
+    use super::xor_buffers;
 
     #[test]
     fn test_hex_to_b64() {
@@ -16,5 +34,15 @@ mod test {
         let have = hex_to_b64(input.as_bytes());
 
         assert_eq!(have.unwrap(), want.into_bytes());
+    }
+
+    #[test]
+    fn text_xor_buffers() {
+        // input
+        let (l, r) = (String::from("1c0111001f010100061a024b53535009181c"), String::from("686974207468652062756c6c277320657965"));
+        let have =  xor_buffers(l.as_bytes(), r.as_bytes());
+        let want = String::from("746865206b696420646f6e277420706c6179").into_bytes();
+
+        assert_eq!(have.unwrap(), hex::decode(&want).unwrap());
     }
 }
