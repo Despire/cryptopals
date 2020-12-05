cf4abb5867d40e1a3be5846867e2b617c6e5bb50
Matúš Mrekaj <matus.mrekaj@icloud.com>
2020-12-05 19:50:58 +0100

    add solution for problem 8 from set 1

--- src/set1.rs
+++ src/set1.rs
@@ -2,6 +2,8 @@ use crypto::buffer::ReadBuffer;
 use crypto::buffer::WriteBuffer;
 use crypto::*;
 
+use std::collections::HashSet;
+
 /// Convert hex to base64
 ///
 /// Solution to problem 1 from set 1: <https://cryptopals.com/sets/1/challenges/1>
@@ -232,10 +234,47 @@ fn decrypt_aes_128_ecb(b: &[u8], k: &[u8]) -> Result<Vec<u8>, crate::Error> {
     Ok(plaintext)
 }
 
+/// Is AES ECB
+///
+/// Solution for problem 8 from set 1: <https://cryptopals.com/sets/1/challenges/8>
+fn detect_aes_128_ecb_mode(b: &[u8]) -> Result<bool, hex::FromHexError> {
+    let b = hex::decode(b)?;
+
+    const block_size: usize = 16; // 128 bits
+
+    if b.len() % block_size != 0 {
+        panic!("unsuported bytes length");
+    }
+
+    let blocks = b.len() / block_size;
+
+    let mut is_ecb = false;
+    let mut seen: HashSet<&[u8]> = HashSet::new();
+    let mut iteration = 0;
+
+    loop {
+        let current = &b[iteration * block_size..(iteration + 1) * block_size];
+        if seen.contains(current) {
+            is_ecb = true;
+            break;
+        }
+
+        seen.insert(current);
+
+        iteration += 1;
+        if iteration == blocks {
+            break;
+        }
+    }
+
+    Ok(is_ecb)
+}
+
 mod test {
     use super::b64_to_hex;
     use super::break_repeating_xor_cipher;
     use super::decrypt_aes_128_ecb;
+    use super::detect_aes_128_ecb_mode;
     use super::find_single_byte_xor;
     use super::hamming_distance;
     use super::hex_to_b64;
@@ -333,5 +372,27 @@ I go crazy when I hear a cymbal",
         let have = decrypt_aes_128_ecb(&bytes, "YELLOW SUBMARINE".as_bytes()).unwrap();
 
         assert_eq!(have, "I\'m back and I\'m ringin\' the bell \nA rockin\' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that\'s my DJ Deshay cuttin\' all them Z\'s \nHittin\' hard and the girlies goin\' crazy \nVanilla\'s on the mike, man I\'m not lazy. \n\nI\'m lettin\' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse\'s to the side yellin\', Go Vanilla Go! \n\nSmooth \'cause that\'s the way I will be \nAnd if you don\'t give a damn, then \nWhy you starin\' at me \nSo get off \'cause I control the stage \nThere\'s no dissin\' allowed \nI\'m in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n\' play \n\nStage 2 -- Yea the one ya\' wanna listen to \nIt\'s off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI\'m an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI\'m like Samson -- Samson to Delilah \nThere\'s no denyin\', You can try to hang \nBut you\'ll keep tryin\' to get my style \nOver and over, practice makes perfect \nBut not if you\'re a loafer. \n\nYou\'ll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I\'m comin\' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin\' \nVanilla Ice is sellin\' and you people are buyin\' \n\'Cause why the freaks are jockin\' like Crazy Glue \nMovin\' and groovin\' trying to sing along \nAll through the ghetto groovin\' this here song \nNow you\'re amazed by the VIP posse. \n\nSteppin\' so hard like a German Nazi \nStartled by the bases hittin\' ground \nThere\'s no trippin\' on mine, I\'m just gettin\' down \nSparkamatic, I\'m hangin\' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n\'89 in my time! You, \'90 is my year. \n\nYou\'re weakenin\' fast, YO! and I can tell it \nYour body\'s gettin\' hot, so, so I can smell it \nSo don\'t be mad and don\'t be sad \n\'Cause the lyrics belong to ICE, You can call me Dad \nYou\'re pitchin\' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don\'t be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you\'re dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n\u{4}\u{4}\u{4}\u{4}".as_bytes());
+    }
+
+    #[test]
+    fn test_is_aes_in_ecb() {
+        let bytes = String::from("d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a").into_bytes();
+        assert_eq!(detect_aes_128_ecb_mode(&bytes).unwrap(), true);
+    }
+
+    #[test]
+    fn find_aes_in_ecb() {
+        let reader = BufReader::new(File::open("./src/mock_data/aes_encrypted.txt").unwrap());
+
+        let mut count = 0;
+        for line in reader.lines() {
+            if detect_aes_128_ecb_mode(&line.unwrap().into_bytes()).unwrap() {
+                break
+            }
+
+            count += 1;
+        }
+
+        assert_eq!(count, 132);
     }
 }
