402c1c2ae1f71f6351a7bff7419081cea602afe8
Matúš Mrekaj <matus.mrekaj@icloud.com>
2020-12-06 19:59:11 +0100

    add solution from problem 2 from set 2

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub enum Error {
     Empty,
 }
 
+pub mod aes_ecb;
 pub mod set1;
 pub mod set2;
--- src/set1.rs
+++ src/set1.rs
@@ -1,18 +1,17 @@
-use crypto::buffer::ReadBuffer;
-use crypto::buffer::WriteBuffer;
-use crypto::*;
+use crypto::buffer::{BufferResult, ReadBuffer, WriteBuffer};
+use crypto::{aes, blockmodes, buffer, symmetriccipher};
 
 use std::collections::HashSet;
 
 /// Convert hex to base64
 ///
 /// Solution to problem 1 from set 1: <https://cryptopals.com/sets/1/challenges/1>
-fn hex_to_b64(b: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
+pub fn hex_to_b64(b: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
     Ok(base64::encode(hex::decode(b)?).into_bytes())
 }
 
 /// helper function to convert from b64 to hex
-fn b64_to_hex(b: &[u8]) -> Result<Vec<u8>, base64::DecodeError> {
+pub fn b64_to_hex(b: &[u8]) -> Result<Vec<u8>, base64::DecodeError> {
     let b: Vec<_> = b.iter().cloned().filter(|&x| x != b'\n').collect();
     let d = base64::decode(b)?;
     let e = hex::encode(d);
@@ -23,7 +22,7 @@ fn b64_to_hex(b: &[u8]) -> Result<Vec<u8>, base64::DecodeError> {
 /// Fixed XOR for two byte buffers
 ///
 /// Solution to problem 2 from set 1: <https://cryptopals.com/sets/1/challenges/2>
-fn xor_buffers(l: &[u8], r: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
+pub fn xor_buffers(l: &[u8], r: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
     assert_eq!(l.len(), r.len());
 
     let l = hex::decode(l)?;
@@ -39,7 +38,7 @@ fn xor_buffers(l: &[u8], r: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
 
 /// letter score is a helper function for problem 3/set1.
 /// scores are assigned from table: <http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html>
-fn letter_score(b: u8) -> f64 {
+pub fn letter_score(b: u8) -> f64 {
     match b {
         b'E' => 12.02,
         b'T' => 9.10,
@@ -73,7 +72,7 @@ fn letter_score(b: u8) -> f64 {
 }
 
 /// score cipher is a helper function for problem 3/set1.
-fn score_cipher(cipher: &[u8]) -> f64 {
+pub fn score_cipher(cipher: &[u8]) -> f64 {
     let mut score = 0.0;
 
     for letter in cipher {
@@ -91,7 +90,7 @@ fn score_cipher(cipher: &[u8]) -> f64 {
 /// Single byte xor cipher
 ///
 /// Solution for problem 3 from set1: <https://cryptopals.com/sets/1/challenges/3>
-fn single_byte_xor(b: &[u8]) -> Result<(f64, u8, Vec<u8>), hex::FromHexError> {
+pub fn single_byte_xor(b: &[u8]) -> Result<(f64, u8, Vec<u8>), hex::FromHexError> {
     let input = hex::decode(b)?;
 
     let (mut high_score, mut message, mut key) = (0.0, Vec::new(), 0 as u8);
@@ -118,7 +117,7 @@ fn single_byte_xor(b: &[u8]) -> Result<(f64, u8, Vec<u8>), hex::FromHexError> {
 /// Find single byte xor
 ///
 /// Solution for problem 4 from set1: <https://cryptopals.com/sets/1/challenges/4>
-fn find_single_byte_xor(b: Vec<Vec<u8>>) -> Result<Vec<u8>, hex::FromHexError> {
+pub fn find_single_byte_xor(b: Vec<Vec<u8>>) -> Result<Vec<u8>, hex::FromHexError> {
     let (mut score, mut result) = (0.0, Vec::new());
 
     for cipher in b {
@@ -136,7 +135,7 @@ fn find_single_byte_xor(b: Vec<Vec<u8>>) -> Result<Vec<u8>, hex::FromHexError> {
 /// Repeating XOR-Key
 ///
 /// Solution for problem 5 from set1: <https://cryptopals.com/sets/1/challenges/5>
-fn repeating_xor_key(b: &[u8]) -> Vec<u8> {
+pub fn repeating_xor_key(b: &[u8]) -> Vec<u8> {
     const KEY: &'static str = "ICE";
 
     let mut result = Vec::new();
@@ -151,7 +150,7 @@ fn repeating_xor_key(b: &[u8]) -> Vec<u8> {
 /// hamming distnace is a helper function is a helper function for solving problem 6.
 ///
 /// Calculates the number of bits that differ.
-fn hamming_distance(b1: &[u8], b2: &[u8]) -> Result<usize, hex::FromHexError> {
+pub fn hamming_distance(b1: &[u8], b2: &[u8]) -> Result<usize, hex::FromHexError> {
     let bits = xor_buffers(b1, b2)?;
 
     let mut count = 0;
@@ -172,7 +171,7 @@ fn hamming_distance(b1: &[u8], b2: &[u8]) -> Result<usize, hex::FromHexError> {
 /// Break repeating XOR-cipher
 ///
 /// Solution for problem 6 from set1: <https://cryptopals.com/sets/1/challenges/6>
-fn break_repeating_xor_cipher(b: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
+pub fn break_repeating_xor_cipher(b: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
     let (mut score, mut size) = (1000, 0);
     // Find the keysize.
     for key_size in 2..41 {
@@ -203,41 +202,41 @@ fn break_repeating_xor_cipher(b: &[u8]) -> Result<Vec<u8>, hex::FromHexError> {
 /// AES in ECB mode
 ///
 /// Solution for problem 7 from set 1: <https://cryptopals.com/sets/1/challenges/7>
-fn decrypt_aes_128_ecb(b: &[u8], k: &[u8]) -> Result<Vec<u8>, crate::Error> {
+pub fn decrypt_aes_128_ecb(
+    b: &[u8],
+    k: &[u8],
+) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
     let mut decryptor = aes::ecb_decryptor(aes::KeySize::KeySize128, k, blockmodes::NoPadding);
 
-    let mut plaintext = Vec::new();
-    let mut read_buffer = buffer::RefReadBuffer::new(&b);
+    let mut final_result = Vec::<u8>::new();
+    let mut read_buffer = buffer::RefReadBuffer::new(b);
     let mut buffer = [0; 4096];
     let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
 
     loop {
-        match decryptor.decrypt(&mut read_buffer, &mut write_buffer, true) {
-            Ok(result) => {
-                plaintext.extend(
-                    write_buffer
-                        .take_read_buffer()
-                        .take_remaining()
-                        .iter()
-                        .map(|&i| i),
-                );
-                match result {
-                    buffer::BufferResult::BufferUnderflow => break,
-                    buffer::BufferResult::BufferOverflow => {}
-                }
-            }
+        let result = decryptor.decrypt(&mut read_buffer, &mut write_buffer, true)?;
 
-            Err(_) => return Err(crate::Error::Empty),
+        final_result.extend(
+            write_buffer
+                .take_read_buffer()
+                .take_remaining()
+                .iter()
+                .map(|&i| i),
+        );
+
+        match result {
+            BufferResult::BufferUnderflow => break,
+            BufferResult::BufferOverflow => {}
         }
     }
 
-    Ok(plaintext)
+    Ok(final_result)
 }
 
 /// Is AES ECB
 ///
 /// Solution for problem 8 from set 1: <https://cryptopals.com/sets/1/challenges/8>
-fn detect_aes_128_ecb_mode(b: &[u8]) -> Result<bool, hex::FromHexError> {
+pub fn detect_aes_128_ecb_mode(b: &[u8]) -> Result<bool, hex::FromHexError> {
     let b = hex::decode(b)?;
 
     const block_size: usize = 16; // 128 bits
@@ -366,15 +365,6 @@ I go crazy when I hear a cymbal",
     }
 
     #[test]
-    fn test_decrypt_aes_in_ecb() {
-        let bytes = base64::decode("CRIwqt4+szDbqkNY+I0qbDe3LQz0wiw0SuxBQtAM5TDdMbjCMD/venUDW9BLPEXODbk6a48oMbAY6DDZsuLbc0uR9cp9hQ0QQGATyyCESq2NSsvhx5zKlLtzdsnfK5ED5srKjK7Fz4Q38/ttd+stL/9WnDzlJvAo7WBsjI5YJc2gmAYayNfmCW2lhZE/ZLG0CBD2aPw0W417QYb4cAIOW92jYRiJ4PTsBBHDe8o4JwqaUac6rqdi833kbyAOV/Y2RMbN0oDb9Rq8uRHvbrqQJaJieaswEtMkgUt3P5Ttgeh7J+hE6TR0uHot8WzHyAKNbUWHoi/5zcRCUipvVOYLoBZXlNu4qnwoCZRSBgvCwTdz3Cbsp/P2wXB8tiz6l9rL2bLhBt13Qxyhhu0H0+JKj6soSeX5ZD1Rpilp9ncR1tHW8+uurQKyXN4xKeGjaKLOejr2xDIw+aWF7GszU4qJhXBnXTIUUNUfRlwEpS6FZcsMzemQF30ezSJHfpW7DVHzwiLyeiTJRKoVUwo43PXupnJXDmUysCa2nQz/iEwyor6kPekLv1csm1Pa2LZmbA9Ujzz8zb/gFXtQqBAN4zA8/wt0VfoOsEZwcsaLOWUPtF/Ry3VhlKwXE7gGH/bbShAIKQqMqqUkEucZ3HPHAVp7ZCn3Ox6+c5QJ3Uv8V7L7SprofPFN6F+kfDM4zAc59do5twgDoClCbxxG0L19TBGHiYP3CygeY1HLMrX6KqypJfFJW5O9wNIF0qfOC2lWFgwayOwq41xdFSCW0/EBSc7cJw3N06WThrW5LimAOt5L9c7Ik4YIxu0K9JZwAxfcU4ShYu6euYmWLP98+qvRnIrXkePugS9TSOJOHzKUoOcb1/KYd9NZFHEcp58Df6rXFiz9DSq80rR5Kfs+M+Vuq5Z6zY98/SP0A6URIr9NFu+Cs9/gf+q4TRwsOzRMjMQzJL8f7TXPEHH2+qEcpDKz/5pE0cvrgHr63XKu4XbzLCOBz0DoFAw3vkuxGwJq4Cpxkt+eCtxSKUzNtXMn/mbPqPl4NZNJ8yzMqTFSODS4bYTBaN/uQYcOAF3NBYFd5x9TzIAoW6ai13a8h/s9i5FlVRJDe2cetQhArrIVBquF0L0mUXMWNPFKkaQEBsxpMCYh7pp7YlyCNode12k5jY1/lc8jQLQJ+EJHdCdM5t3emRzkPgND4a7ONhoIkUUS2R1oEV1toDj9iDzGVFwOvWyt4GzA9XdxT333JU/n8m+N6hs23MBcZ086kp9rJGVxZ5f80jRz3ZcjU6zWjR9ucRyjbsuVn1t4EJEm6A7KaHm13m0vwN/O4KYTiiY3aO3siayjNrrNBpn1OeLv9UUneLSCdxcUqjRvOrdA5NYv25Hb4wkFCIhC/Y2ze/kNyis6FrXtStcjKC1w9Kg8O25VXB1Fmpu+4nzpbNdJ9LXahF7wjOPXN6dixVKpzwTYjEFDSMaMhaTOTCaqJig97624wv79URbCgsyzwaC7YXRtbTstbFuEFBee3uW7B3xXw72mymM2BS2uPQ5NIwmacbhta8aCRQEGqIZ078YrrOlZIjar3lbTCo5o6nbbDq9bvilirWG/SgWINuc3pWl5CscRcgQQNp7oLBgrSkQkv9AjZYcvisnr89TxjoxBO0Y93jgp4T14LnVwWQVx3l3d6S1wlscidVeaM24E/JtS8k9XAvgSoKCjyiqsawBMzScXCIRCk6nqX8ZaJU3rZ0LeOMTUw6MC4dC+aY9SrCvNQub19mBdtJUwOBOqGdfd5IoqQkaL6DfOkmpnsCs5PuLbGZBVhah5L87IY7r6TB1V7KboXH8PZIYc1zlemMZGU0o7+etxZWHgpdeX6JbJIs3ilAzYqw/Hz65no7eUxcDg1aOaxemuPqnYRGhW6PvjZbwAtfQPlofhB0jTHt5bRlzF17rn9q/6wzlc1ssp2xmeFzXoxffpELABV6+yj3gfQ/bxIB9NWjdZK08RX9rjm9CcBlRQeTZrD67SYQWqRpT5t7zcVDnx1s7ZffLBWm/vXLfPzMaQYEJ4EfoduSutjshXvR+VQRPs2TWcF7OsaE4csedKUGFuo9DYfFIHFDNg+1PyrlWJ0J/X0PduAuCZ+uQSsM/ex/vfXp6Z39ngq4exUXoPtAIqafrDMd8SuAtyEZhyY9V9Lp2qNQDbl6JI39bDz+6pDmjJ2jlnpMCezRK89cG11IqiUWvIPxHjoiT1guH1uk4sQ2Pc1J4zjJNsZgoJDcPBbfss4kAqUJvQyFbzWshhtVeAv3dmgwUENIhNK/erjpgw2BIRayzYw001jAIF5c7rYg38o6x3YdAtU3d3QpuwG5xDfODxzfL3yEKQr48C/KqxI87uGwyg6H5gc2AcLU9JYt5QoDFoC7PFxcE3RVqc7/Um9Js9X9UyriEjftWt86/tEyG7F9tWGxGNEZo3MOydwX/7jtwoxQE5ybFjWndqLp8DV3naLQsh/Fz8JnTYHvOR72vuiw/x5D5PFuXV0aSVvmw5Wnb09q/BowS14WzoHH6ekaWbh78xlypn/L/M+nIIEX1Ol3TaVOqIxvXZ2sjm86xRz0EdoHFfupSekdBULCqptxpFpBshZFvauUH8Ez7wA7wjL65GVlZ0f74U7MJVu9SwsZdgsLmnsQvr5n2ojNNBEv+qKG2wpUYTmWRaRc5EClUNfhzh8iDdHIsl6edOewORRrNiBay1NCzlfz1cj6VlYYQUM9bDEyqrwO400XQNpoFOxo4fxUdd+AHmCBhHbyCR81/C6LQTG2JQBvjykG4pmoqnYPxDyeiCEG+JFHmP1IL+jggdjWhLWQatslrWxuESEl3PEsrAkMF7gt0dBLgnWsc1cmzntG1rlXVi/Hs2TAU3RxEmMSWDFubSivLWSqZj/XfGWwVpP6fsnsfxpY3d3h/fTxDu7U8GddaFRQhJ+0ZOdx6nRJUW3u6xnhH3mYVRk88EMtpEpKrSIWfXphgDUPZ0f4agRzehkn9vtzCmNjFnQb0/shnqTh4Mo/8oommbsBTUKPYS7/1oQCi12QABjJDt+LyUan+4iwvCi0k0IUIHvk21381vC0ixYDZxzY64+xx/RNID+iplgzq9PDZgjc8L7jMg+2+mrxPS56e71m5E2zufZ4d+nFjIg+dHD/ShNPzVpXizRVUERztLuak8Asah3/yvwOrH1mKEMMGC1/6qfvZUgFLJH5V0Ep0n2K/Fbs0VljENIN8cjkCKdG8aBnefEhITdV7CVjXcivQ6efkbOQCfkfcwWpaBFC8tD/zebXFE+JshW16D4EWXMnSm/9HcGwHvtlAj04rwrZ5tRvAgf1IR83kqqiTvqfENcj7ddCFwtNZrQK7EJhgB5Tr1tBFcb9InPRtS3KYteYHl3HWR9t8E2YGE8IGrS1sQibxaK/C0kKbqIrKpnpwtoOLsZPNbPw6K2jpko9NeZAx7PYFmamR4D50KtzgELQcaEsi5aCztMg7fp1mK6ijyMKIRKwNKIYHagRRVLNgQLg/WTKzGVbWwq6kQaQyArwQCUXo4uRtyzGMaKbTG4dns1OFB1g7NCiPb6s1lv0/lHFAF6HwoYV/FPSL/pirxyDSBb/FRRA3PIfmvGfMUGFVWlyS7+O73l5oIJHxuaJrR4EenzAu4Avpa5d+VuiYbM10aLaVegVPvFn4pCP4U/Nbbw4OTCFX2HKmWEiVBB0O3J9xwXWpxN1Vr5CDi75FqNhxYCjgSJzWOUD34Y1dAfcj57VINmQVEWyc8Tch8vg9MnHGCOfOjRqp0VGyAS15AVD2QS1V6fhRimJSVyT6QuGb8tKRsl2N+a2Xze36vgMhw7XK7zh//jC2H").unwrap();
-
-        let have = decrypt_aes_128_ecb(&bytes, "YELLOW SUBMARINE".as_bytes()).unwrap();
-
-        assert_eq!(have, "I\'m back and I\'m ringin\' the bell \nA rockin\' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that\'s my DJ Deshay cuttin\' all them Z\'s \nHittin\' hard and the girlies goin\' crazy \nVanilla\'s on the mike, man I\'m not lazy. \n\nI\'m lettin\' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse\'s to the side yellin\', Go Vanilla Go! \n\nSmooth \'cause that\'s the way I will be \nAnd if you don\'t give a damn, then \nWhy you starin\' at me \nSo get off \'cause I control the stage \nThere\'s no dissin\' allowed \nI\'m in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n\' play \n\nStage 2 -- Yea the one ya\' wanna listen to \nIt\'s off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI\'m an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI\'m like Samson -- Samson to Delilah \nThere\'s no denyin\', You can try to hang \nBut you\'ll keep tryin\' to get my style \nOver and over, practice makes perfect \nBut not if you\'re a loafer. \n\nYou\'ll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I\'m comin\' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin\' \nVanilla Ice is sellin\' and you people are buyin\' \n\'Cause why the freaks are jockin\' like Crazy Glue \nMovin\' and groovin\' trying to sing along \nAll through the ghetto groovin\' this here song \nNow you\'re amazed by the VIP posse. \n\nSteppin\' so hard like a German Nazi \nStartled by the bases hittin\' ground \nThere\'s no trippin\' on mine, I\'m just gettin\' down \nSparkamatic, I\'m hangin\' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n\'89 in my time! You, \'90 is my year. \n\nYou\'re weakenin\' fast, YO! and I can tell it \nYour body\'s gettin\' hot, so, so I can smell it \nSo don\'t be mad and don\'t be sad \n\'Cause the lyrics belong to ICE, You can call me Dad \nYou\'re pitchin\' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don\'t be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you\'re dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n\u{4}\u{4}\u{4}\u{4}".as_bytes());
-    }
-
-    #[test]
     fn test_is_aes_in_ecb() {
         let bytes = String::from("d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a").into_bytes();
         assert_eq!(detect_aes_128_ecb_mode(&bytes).unwrap(), true);
@@ -387,7 +377,7 @@ I go crazy when I hear a cymbal",
         let mut count = 0;
         for line in reader.lines() {
             if detect_aes_128_ecb_mode(&line.unwrap().into_bytes()).unwrap() {
-                break
+                break;
             }
 
             count += 1;
